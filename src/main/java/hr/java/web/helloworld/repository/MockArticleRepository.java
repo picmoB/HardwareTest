555e05e4109b48d99c4017abab5c6395113777c6
IvanM <ivan.munivrana99@gmail.com>
2026-09-24 19:43:27 +0200

    jdbc_update

--- src/main/java/hr/java/web/helloworld/repository/MockArticleRepository.java
+++ src/main/java/hr/java/web/helloworld/repository/MockArticleRepository.java
@@ -41,7 +41,7 @@ public class MockArticleRepository implements ArticleRepository {
     }
 
     @Override
-    public Optional<Hardware> getArticlesById(Integer brandId) {
+    public List<Hardware> getArticlesById(Integer brandId) {
         return hardwareList.stream()
                 .filter(a -> a.getId().equals(brandId))
                 .collect(Collectors.toList());
@@ -49,7 +49,7 @@ public class MockArticleRepository implements ArticleRepository {
     }
 
     @Override
-    public Hardware saveNewArticle(Hardware hardware) {
+    public int saveNewArticle(Hardware hardware) {
         if (hardwareList.contains(hardware)) {
             return hardwareList.indexOf(hardware);
         } else  {
